4da797ba8c80f81b4b572aaf9cf3b5d54fccf312
github username <github email address>
2022-12-29 22:00:22 -0500

    changes made

---
+++ playground/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path 
+from . import views
+
+
+urlpatterns = [
+    path('playground/hello',views.say_hello)
+]
--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# Views In Django Are More of a request handler , like controllers in Ruby 
+# request - > response
+
+
+def say_hello(request):
+    return HttpResponse('Hello From Django!!!')
