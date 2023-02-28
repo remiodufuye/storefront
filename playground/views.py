7f373489ceb6115853333be8885762416580c8f0
github username <github email address>
2023-02-27 22:17:15 -0500

    changes made

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
-from django.http import HttpResponse
+from django.db.models import Q
 from store.models import Product 
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(inventory__lt=10)
+    queryset = Product.objects.filter(Q(inventory__lt=10)| Q(unit_price__lt=20))
     return render(request, 'hello.html', {'name': 'Rado','products':list(queryset)})
 
