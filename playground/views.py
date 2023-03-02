838d8d975ec1d2f3e2ca0d6c75fbcbc783c7ec46
github username <github email address>
2023-03-01 21:10:24 -0500

    changes made

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.db.models import Q,F
-from store.models import Product 
+# from django.db.models import Q,F
+from store.models import Product ,OrderItem
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(inventory=F('collection__id'))
+    queryset = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
     return render(request, 'hello.html', {'name': 'Rado','products':list(queryset)})
 
