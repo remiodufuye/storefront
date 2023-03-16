a7c12ead140c407bbefe773a18529cad430760c3
github username <github email address>
2023-03-15 21:57:39 -0400

    Changes made

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.db.models import Count ,Min
-from store.models import Product ,Order
+from django.db.models import Value , F
+from store.models import Customer 
 
 
 def say_hello(request):
-    result = Order.objects.aggregate(count =Count('id'))
-    return render(request, 'hello.html', {'name': 'Rado','result':result})
+    queryset = Customer.objects.annotate(new_id=F('id') + 1 )
+    return render(request, 'hello.html', {'name': 'Rado','result':list(queryset)})
 
