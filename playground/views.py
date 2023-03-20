099cfbd573f07cb43504bde3e5bc2f02f1fe89e8
github username <github email address>
2023-03-19 21:47:24 -0400

    changes made

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,12 @@
+
 from django.shortcuts import render
-from django.db.models import Value , F
-from store.models import Customer 
+from django.db.models import Value 
+from django.db.models.functions import Concat  
+from store.models import Product 
 
 
 def say_hello(request):
-    queryset = Customer.objects.annotate(new_id=F('id') + 1 )
+
+    queryset = Product.objects.filter(unit_price__gt=100)
     return render(request, 'hello.html', {'name': 'Rado','result':list(queryset)})
 
